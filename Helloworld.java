c3d135d20b5ef013974caf288e7e6d4905463493
JohnAu1231 <john012938@yahoo.com.hk>
2024-03-26 10:30:46 +0800

    update

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+  public static void main(String[] args){
+    // Start point of the program
+    // Tool 1: System.out.println() 印出文字
+    System.out.println("Hello world, I'm John Au, 你好");
+  }
+  
+}
