fb67d592e3044b34783889724051467ca6bd8a11
JohnAu1231 <john012938@yahoo.com.hk>
2024-05-14 17:28:12 +0800

    update

--- bc2403/furniture-factory2/src/main/java/com/javahongkong/bootcamp/exercise/Furniture.java
+++ bc2403/furniture-factory2/src/main/java/com/javahongkong/bootcamp/exercise/Furniture.java
@@ -1,11 +1,10 @@
 package com.javahongkong.bootcamp.exercise;
 
-import lombok.Getter;
 
 /**
  * FURNITURE_TYPE("Furniture Name", floating-point cost)
  */
-@Getter
+
 public enum Furniture {
 	CHAIR("Chair", 100.0f),
 	TABLE("Table", 250.0f),
